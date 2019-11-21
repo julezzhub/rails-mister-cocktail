e468e42e876d9b162e46e9cdf07baf19c43eee04
Julius Hillebrand <hillebrandjulius@gmail.com>
2019-11-21 12:49:46 +0200

    Pushing recent changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Dose.destroy_all
+puts "Destroying all doses"
+
+Ingredient.destroy_all
+puts "Destroying all ingredients"
+
+Cocktail.destroy_all
+puts "Destroying all cocktails"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+serialized_ingredients = open(url).read
+
+ingredients = JSON.parse(serialized_ingredients)
+
+ingredients['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+Ingredient.create(name: 'Ginger Beer')
+
+Cocktail.create!(name: 'Moquito')
+Cocktail.create!(name: 'Sex on the Beach')
+Cocktail.create!(name: 'Moscow Mule')
+
+Dose.create!(
+  description: '6cl',
+  cocktail_id: Cocktail.find_by(name: 'Moscow Mule').id,
+  ingredient_id: Ingredient.find_by(name: 'Vodka').id
+)
+
+Dose.create!(
+  description: '9cl',
+  cocktail_id: Cocktail.find_by(name: 'Moscow Mule').id,
+  ingredient_id: Ingredient.find_by(name: 'Ginger Beer').id
+)
